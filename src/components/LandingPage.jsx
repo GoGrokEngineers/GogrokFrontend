ed61518aa6b36382f0ddd54c370b173db5596cf6
Positiveoo1 <github0512@gmail.com>
2025-05-22 08:58:16 +0200

    landingpage enhanced design

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,9 +1,9 @@
+import React, { useState } from "react";
 import { Box, Button, Card, Typography } from "@mui/material";
 import OTPInput from "./ui/opt-input";
 import { useNavigate } from "react-router-dom";
 import { Bounce, toast } from "react-toastify";
-import { useState } from "react";
-import WebsocketServices from "../services/web-socket";
+import { motion } from "framer-motion";
 
 const MatchingRoom = () => {
   const [inputValue, setInputValue] = useState("");
@@ -65,23 +65,22 @@ const MatchingRoom = () => {
         alignItems={"center"}
         flexDirection={"column"}
         height={"100vh"}
+        position="relative"
       >
-        <Box sx={{ display: "flex" }}>
-          <Typography
-            sx={{
-              fontSize: "80px",
-              fontWeight: "800",
-              color: "#ffff",
-              userSelect: "none",
-              "& span": {
-                color: "#F8B179",
-              },
-            }}
-          >
-            Go
-            <span>Grok</span>
-          </Typography>
-        </Box>
+        <Typography
+          sx={{
+            fontSize: "80px",
+            fontWeight: "800",
+            color: "#ffff",
+            userSelect: "none",
+            "& span": {
+              color: "#F8B179",
+            },
+          }}
+        >
+          Go<span>Grok</span>
+        </Typography>
+
         <Card
           sx={{
             marginY: "20px",
@@ -100,14 +99,25 @@ const MatchingRoom = () => {
             }}
             component="form"
           >
-            {/* OTP Input for numbers */}
+                {/* Animated Arrow + Text for Join */}
+            {inputValue.length < 6 && (
+              <motion.div
+                animate={{ y: [0, -10, 0] }}
+                transition={{ repeat: Infinity, duration: 1.2 }}
+                style={{ color: "#F8B179", marginTop: "10px", fontSize: "1rem" }}
+              >
+                ↓ Enter code to join
+              </motion.div>
+            )}
             <OTPInput
               value={inputValue}
               onChange={setInputValue}
               separator={<span>-</span>}
               length={6}
             />
-            {/* Join button */}
+
+        
+
             <Button
               type="submit"
               sx={{
@@ -125,13 +135,22 @@ const MatchingRoom = () => {
           </Box>
         </Card>
 
-        {/* Create button becomes disabled if any digit is entered */}
+        {/* Animated Arrow + Text for Create */}
+        {inputValue.length === 0 && (
+          <motion.div
+            animate={{ y: [0, -10, 0] }}
+            transition={{ repeat: Infinity, duration: 1.2 }}
+            style={{ color: "#F8B179", fontSize: "1rem", marginBottom: "5px" }}
+          >
+            ↓ Or create your own room
+          </motion.div>
+        )}
+
         <Button
           onClick={handleCreate}
           disabled={inputValue.length > 0}
           sx={{
             width: "140px",
-            marginTop: "35px",
             backgroundColor: "#ffff",
             fontSize: "20px",
             fontWeight: 600,
@@ -145,4 +164,5 @@ const MatchingRoom = () => {
     </div>
   );
 };
+
 export default MatchingRoom;
