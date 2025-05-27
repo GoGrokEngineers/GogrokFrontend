f6dcb59a37bd5dd4816c4f495eff51b89c994bc7
Positiveoo1 <github0512@gmail.com>
2025-05-27 10:15:19 +0200

    integration enhnaced

--- src/hooks/useCreateCompetition.js
+++ src/hooks/useCreateCompetition.js
@@ -1,8 +1,13 @@
 import { useMutation } from "@tanstack/react-query";
 import ApiService from "../services/api-serives";
+import { toast } from "react-toastify";
 
 export const useCreateCompetition = () => {
-  return useMutation({ mutationFn: ApiService.createCompetition });
+  return useMutation({
+    mutationFn: ApiService.createCompetition,
+    onError: (error) => {
+      console.error("CreateCompetition Error:", error);
+      toast.warn(/* … */);
+    }
+  });
 };
-
-
